3c1e8a086654c8af8fc9e0e03f8e1e84183758f1
Dhruv Gargi <dhruv.gargi@learner.manipal.edu>
2019-03-27 16:21:09 +0000

    Automatically backed up by Learn

--- try.rb
+++ try.rb
@@ -1,4 +1,10 @@
 board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 def turn_count(board)
-puts board
+counter = 9
+board.each do |a|
+if a =="" || a == " "
+counter = counter - 1
+end
+end
+return counter
 end
